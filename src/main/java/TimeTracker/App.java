c3e7ab616cd6d3e7041a5e16ba391949f3de936b
Dinesh Mudrakola <dineshmudrakola94@gmail.com>
2018-10-06 21:47:25 -0400

    first commit

---
+++ src/main/java/TimeTracker/App.java
@@ -0,0 +1,13 @@
+package TimeTracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
